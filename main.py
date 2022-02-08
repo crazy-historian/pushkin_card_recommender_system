e5543cf6c05ae6ad95473213a84161b55bd11a79
Максим <zaitsev808@mail.ru>
2022-02-08 14:53:32 +0300

    uploaded prepared user_events dataframes .csv files and recommendations .json files for users in regions; added the main.py file which runs recommendation calculation.

---
+++ main.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import preprocessing.data_management as dm
+
+from pathlib import Path
+from recommenders.implicit_models import EventRecommender
+
+if __name__ == "__main__":
+    for user_event_df_file_path in Path('dataset/user_event_dfs').iterdir():
+        region_name = user_event_df_file_path.stem.split('_')[0]
+
+        print(f'{region_name}_user_event dataframe is being processed ...')
+        user_event_df = pd.read_csv(user_event_df_file_path)
+        user_event_df.drop(columns='Unnamed: 0', inplace=True)
+        extra_event_ids = dm.get_extra_events_ids(user_event_df)
+
+        print('\t - preparing recommendations.')
+        recommender = EventRecommender(user_event_df, extra_event_ids=extra_event_ids, model_name='als')
+        recommender.prepare_recommendations()
+
+        print('\t - saving as .json.')
+        recommender.save_as_json(f"recommendations/{region_name}_rec.json")
