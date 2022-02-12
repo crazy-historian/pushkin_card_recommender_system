6da9e03c397a8b5254f44c1e17a5e97010ec5797
Emil Fattakhov <fattahov_emil@mail.ru>
2022-02-12 23:42:16 +0500

    Добавлен путь для сохранения данных из API в json файл

--- sandbox/config.py
+++ sandbox/config.py
@@ -1,14 +1,16 @@
-REGION_FILE_PATH = './main_data/region.txt'
-REGION_NUMS_FILE_PATH = './main_data/RegionRussia.csv'
-USERS_FILE_PATH = './main_data/users.txt'
-UNIQ_FILE_PATH = './main_data/uniq.txt'
-CLICK_FILE_PATH = './main_data/click.txt'
+REGION_FILE_PATH = './../main_data/region.txt'
+REGION_NUMS_FILE_PATH = './../main_data/RegionRussia.csv'
+USERS_FILE_PATH = './../main_data/users.txt'
+UNIQ_FILE_PATH = './../main_data/uniq.txt'
+CLICK_FILE_PATH = './../main_data/click.txt'
 
-ALL_EVENTS_FILE_PATH = './main_data/events/events_pushka_accepted_30122021.csv'
-EVENTS_FILE_PATH = './main_data/events/events.csv'
-ORGANIZATIONS_FILE_PATH = './main_data/events/organizations_updated.csv'
+ALL_EVENTS_FILE_PATH = './../main_data/events/events_pushka_accepted_30122021.csv'
+EVENTS_FILE_PATH = './../main_data/events/events.csv'
+ORGANIZATIONS_FILE_PATH = './../main_data/events/organizations_updated.csv'
+
+REQUEST_FILE_PATH = './../main_data/request_all.json'
 
 #path to save datasets
-MAIN_PATH = './main_data/'
+MAIN_PATH = './../main_data/'
 
 DOWNLOAD_DATE = '2021-11-15'
