598503246dfa2ecce3d8936aca404846656d9e45
crazy_historian <zaitsev808@mail.ru>
2022-03-23 19:04:35 +0300

    delete 'sudo' command in bash script.

--- run_docker.sh
+++ run_docker.sh
@@ -1,4 +1,4 @@
-sudo docker build -t work/pushkin_card_recommender_system -f ./Dockerfile . && \
-sudo docker run --rm -it \
+docker build -t work/pushkin_card_recommender_system -f ./Dockerfile . && \
+docker run --rm -it \
   -v $(pwd)/app:/app -v $(pwd)/data:/data \
   work/pushkin_card_recommender_system bash run.sh
